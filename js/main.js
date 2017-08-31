79db92bfcdb81d26dac0c159950495e0bf776e2b
Kongdumri <kongdumri.kdr@g.swu.ac.th>
2017-08-31 16:27:57 +0700

    Add work

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function go(key){
+    let a = document.getElementById('output').textContent;
+    a += key ;
+    document.getElementById('output').textContent = a; 
+}
+function backspace() {
+    var out = document.getElementById("output").textContent;
+    out = out.substring(0, out.length - 1);
+    document.getElementById("output").textContent = out;
+}
